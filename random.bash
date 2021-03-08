44c2160ff340dd0c543344985edf6da93bafe1c8
ratman00 <32817032+ratman00@users.noreply.github.com>
2021-03-08 16:38:47 -0600

    Add files via upload

--- random.bash
+++ random.bash
@@ -1,24 +1,24 @@
-#!/bin/bash
 # generic boiler plate stuff. sets the shell to be used. probably shouldn't be changed.
+#!/bin/bash
 
+# prompts for a filename. will also be used to select output file format.
 echo "output filename:"
 read filename
-# prompts for a filename. will also be used to select output file format.
 
-cd /home/rot/Documents/video-dir
-# enters audio directory. change /home/rot/Documents/video-dir to whatever directory your input video files are.
+# enters audio directory. change ~/Documents/video-dir to whatever directory your input video files are.
+cd ~/Documents/video-dir
 
+# picks a random file from the directory cd entered in the previous line.
 video=$(ls -1d $PWD/*| shuf -n 1)
+
+# enters audio directory. change ~/Documents/video-dir to whatever directory your input audio files are.
+cd ~/Documents/audio-dir
+
 # picks a random file from the directory cd entered in the previous line.
-
-cd /home/rot/Documents/audio-dir
-# enters audio directory. change /home/rot/Documents/video-dir to whatever directory your input audio files are.
-
 audio=$(ls -1d $PWD/* | shuf -n 1)
-# picks a random file from the directory cd entered in the previous line.
 
-cd /home/rot/Documents/output-dir
 # changes location to output-dir. you can change the location to wherever you want.
+cd /home/rot/Documents/output-dir
 
-ffmpeg -i $video -i $audio -c:v copy -map 0:v:0 -map 1:a:0 $filename
 # takes $video and $audio and combines them into $filename. video will not be re-encoded to save time, but audio will to prevent issues.
+ffmpeg -i $video -i $audio -c:v copy -map 0:v:0 -map 1:a:0 $filename
